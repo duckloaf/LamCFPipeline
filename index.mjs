ab1192e5065cbfbf95f50b28978d2b32037adc08
Michael <mkrome@teysaust.com.au-at-001113412980>
2025-02-27 11:45:11 +1000

    update

--- index.mjs
+++ index.mjs
@@ -1,7 +1,7 @@
 export const handler = async (event) => {
     const response = {
         statusCode: 200,
-        message: 'This is the initial PRODUCTION deployment of this code.'
+        message: 'This is the initial PRODUCTION deployment of this code....'
     };
     return response;
 };
