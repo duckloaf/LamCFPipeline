ab075c94a5b594ae51f474a812667bd29865c233
Duckloaf <31424178+duckloaf@users.noreply.github.com>
2025-02-27 17:02:50 +1000

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -1,7 +1,7 @@
 export const handler = async (event) => {
     const response = {
         statusCode: 200,
-        message: 'This is an update that should trigger the UAT workflow deployment'
+        message: 'This is an update from the github UI that should trigger the UAT workflow deployment'
     };
     return response;
 };
