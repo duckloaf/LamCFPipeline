a4f3c1e99316e5c7ebf8b436ae110de3a671adf8
Michael <mkrome@teysaust.com.au-at-001113412980>
2025-02-27 17:29:50 +1000

    client update

--- index.mjs
+++ index.mjs
@@ -1,7 +1,7 @@
 export const handler = async (event) => {
     const response = {
         statusCode: 200,
-        message: 'This is an update from the github UI that should hopefully for real this time trigger the UAT workflow deployment to AWS'
+        message: 'This is an update from a developer for UAT'
     };
     return response;
 };
