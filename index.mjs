dec4119cbce01f17c1d95210a88a11b11fdf0dc6
Michael <mkrome@teysaust.com.au-at-001113412980>
2025-02-27 17:21:34 +1000

    please

--- index.mjs
+++ index.mjs
@@ -1,7 +1,7 @@
 export const handler = async (event) => {
     const response = {
         statusCode: 200,
-        message: 'This is an update from the github UI that should for real this time trigger the UAT workflow deployment to AWS'
+        message: 'This is an update from the github UI that should hopefully for real this time trigger the UAT workflow deployment to AWS'
     };
     return response;
 };
