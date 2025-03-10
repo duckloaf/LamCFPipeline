2e9e8b9fd0b77408e18e0d841adc8c06d0f92fad
Michael <mkrome@teysaust.com.au-at-001113412980>
2025-03-10 14:56:03 +1000

    h

--- index.mjs
+++ index.mjs
@@ -1,7 +1,7 @@
 export const handler = async (event) => {
     const response = {
         statusCode: 200,
-        message: 'This is an update on the develop branch for UAT - full cicd 2g'
+        message: 'This is an update on the develop branch for UAT - full cicd 2h'
     };
     return response;
 };
