3acef23e5b603062755f3d19b28e520040a48cf7
Michael <mkrome@teysaust.com.au-at-001113412980>
2025-03-10 11:14:50 +1000

    update action and trigger

--- index.mjs
+++ index.mjs
@@ -1,7 +1,7 @@
 export const handler = async (event) => {
     const response = {
         statusCode: 200,
-        message: 'This is an update on the develop branch for UAT - 3'
+        message: 'This is an update on the develop branch for UAT - 4'
     };
     return response;
 };
