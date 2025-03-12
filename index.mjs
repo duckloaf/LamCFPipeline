e51dac3739dc1d14a89a4906ce9a01465a05597a
Michael <mkrome@teysaust.com.au-at-001113412980>
2025-03-12 13:57:22 +1000

    trigger bug

--- index.mjs
+++ index.mjs
@@ -1,7 +1,7 @@
 export const handler = async (event) => {
     const response = {
         statusCode: 200,
-        message: 'This is an update on the develop branch for UAT - full cicd 3c'
+        message: 'This is an update on the develop branch for UAT - full cicd txt1'
     };
     return response;
 };
